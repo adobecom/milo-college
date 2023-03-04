623c63c5783f6008425797cb782be8e10752e3f7
Chris Millar <cmillar@adobe.com>
2023-03-03 22:19:17 -0700

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -10,30 +10,33 @@
  * governing permissions and limitations under the License.
  */
 
-/*
- * ------------------------------------------------------------
- * Edit below at your own risk
- * ------------------------------------------------------------
- */
-
 /**
  * The decision engine for where to get Milo's libs from.
  */
 export const [setLibs, getLibs] = (() => {
   let libs;
   return [
-    (prodLibs) => {
-      const { hostname } = window.location;
-//       if (!hostname.includes('hlx.page')
-//         && !hostname.includes('hlx.live')
-//         && !hostname.includes('localhost')) {
-//         libs = prodLibs;
-//         return libs;
-//       }
-      const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
-      if (branch === 'local') return 'http://localhost:6456/libs';
-      if (branch.indexOf('--') > -1) return `https://${branch}.hlx.page/libs`;
-      return `https://${branch}--milo--adobecom.hlx.live/libs`;
+    (prodLibs, location) => {
+      const { hostname, search } = location || window.location;
+      if (!hostname.includes('.hlx.') && !hostname.includes('local')) {
+        libs = prodLibs;
+      } else {
+        const branch = new URLSearchParams(search).get('milolibs') || 'main';
+        if (branch === 'local') {
+          libs = 'http://localhost:6456/libs';
+        } else if (branch.indexOf('--') > -1) {
+          libs = `https://${branch}.hlx.page/libs`;
+        } else {
+          libs = `https://${branch}--milo--adobecom.hlx.page/libs`;
+        }
+      }
+      return libs;
     }, () => libs,
   ];
 })();
+
+/*
+ * ------------------------------------------------------------
+ * Edit above at your own risk
+ * ------------------------------------------------------------
+ */
