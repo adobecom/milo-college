39dc8e607e70ce2f61bc248e3d390cafa24a1920
Chris Millar <cmillar@adobe.com>
2023-03-05 11:01:34 -0700

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -17,18 +17,13 @@ export const [setLibs, getLibs] = (() => {
   let libs;
   return [
     (prodLibs, location) => {
-      const { hostname, search } = location || window.location;
-      if (!(hostname.includes('.hlx.') || hostname.includes('local'))) {
-        libs = prodLibs;
-      } else {
+      libs = (() => {
+        const { hostname, search } = location || window.location;
+        if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
-        if (branch === 'local') {
-          libs = 'http://localhost:6456/libs';
-        } else {
-          const sub = branch.includes('--') ? branch : `${branch}--milo--adobecom`;
-          libs = `https://${sub}.hlx.live/libs`;
-        }
-      }
+        if (branch === 'local') return 'http://localhost:6456/libs';
+        return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
+      })();
       return libs;
     }, () => libs,
   ];
