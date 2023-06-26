30c88b8b65f0645f24ff300a67b6ac0617845d5c
Chris Millar <cmillar@adobe.com>
2023-06-25 20:46:52 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,8 +15,8 @@ import { setLibs } from './utils.js';
 // Add project-wide style path here.
 const STYLES = '';
 
-// Use '/libs' if your live site maps '/libs' to milo's origin.
-const LIBS = 'https://milo.adobe.com/libs';
+// Use 'https://milo.adobe.com/libs' if you cannot map '/libs' to milo's origin.
+const LIBS = '/libs';
 
 // Add any config options.
 const CONFIG = {
